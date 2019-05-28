6be2ce9ec4ef7f532859f2c47450f3b6dda035ae
andluo <1455234749@qq.com>
2019-05-29 00:35:27 +0800

    完成星星的样式

--- src/js/mod/note.js
+++ src/js/mod/note.js
@@ -24,7 +24,14 @@ note.prototype = {
                     <span class="iconfont icon-bianji" data-id=${this.id}></span>
                 </div>
                 <div class="bottom">
-                <div class="grade"></div>
+                <div class="grade">
+                        <span class="iconfont icon-xingxingman"></span>
+                        <span class="iconfont icon-xingxingman"></span>
+                        <span class="iconfont icon-xingxingman"></span>
+                        <span class="iconfont icon-xingxingman"></span>
+                        <span class="iconfont icon-xingxingman"></span>
+
+                </div>
                 <div data-id=${this.id} class="${this.status===0 ?'uncompleted':'completed'}">${this.status===0?'未完成':'<span class="iconfont icon-gouxuan"></span>'}</div>
                 </div>
             </li>`
