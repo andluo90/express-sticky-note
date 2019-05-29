574ffc2a5b206ad26e8126670a162aa0d88372a5
andluo <1455234749@qq.com>
2019-05-30 00:53:43 +0800

    基本完成所有功能

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -12,7 +12,21 @@ waterfall()
     //修复bug
     const right = $('header .wrapper').css('marginLeft') //计算添加按钮距离页面右边的距离
     $('.addNote').css({right:right})
+    console.log(`添加按钮距离页面右边的距离为${right}`)
 }()
+
+//计算页面的高度后调整main元素的高度
+
+!function(){
+    setTimeout(()=>{
+        const scrollHeight = $(document).height()
+        $('main').css({height:`${scrollHeight}px`})
+        console.log(`main的高度为${scrollHeight}px`)
+    },3000)
+    
+
+}()
+
     
     
 
